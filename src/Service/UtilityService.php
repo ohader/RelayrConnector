0cb5df84e4f062c4202870d005e238b13cf36cee
Oliver Hader <oliver@typo3.org>
2015-02-01 18:49:43 +0100

    [TASK] Integrate utility to get sensor model and reading assignments

---
+++ src/Service/UtilityService.php
@@ -0,0 +1,54 @@
+<?php
+namespace OliverHader\RelayrConnector\Service;
+
+use \OliverHader\RelayrConnector as Relayr;
+
+class UtilityService {
+
+	/**
+	 * @var UtilityService
+	 */
+	static protected $instance;
+
+	/**
+	 * @return UtilityService
+	 */
+	static public function getInstance() {
+		if (!isset(static::$instance)) {
+			static::$instance = new UtilityService();
+		}
+		return static::$instance;
+	}
+
+	/**
+	 * @param array $values
+	 * @param \ArrayObject|Relayr\Model\Reading[] $readings
+	 * @return array|Relayr\Model\Reading[]
+	 */
+	public function determineReadingAssignment(array $values, \ArrayObject $readings = NULL) {
+		if ($readings === NULL) {
+			return array();
+		}
+
+		$readings = $readings->getArrayCopy();
+		$readingsAssignment = array();
+
+		foreach ($values as $key => $value) {
+			/** @var Relayr\Model\Reading $reading */
+			foreach ($readings as $index => $reading) {
+				if (strpos($reading->getMeaning(), $key) === 0) {
+					$readingsAssignment[$key] = $reading;
+					unset($values[$key]);
+					unset($readings[$index]);
+				}
+			}
+		}
+
+		foreach ($values as $key => $value) {
+			$readingsAssignment[$key] = array_shift($readings);
+		}
+
+		return $readingsAssignment;
+	}
+
+}
